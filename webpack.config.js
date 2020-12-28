9332d4f1c04d2b022907c9df469f9a4f31931486
Josep Seto <jsetojseto@gmail.com>
2020-12-28 20:45:46 +0100

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = ( env, arg ) => {
+	return {
+		mode: 'development',
+		entry: {
+			'crypto-bot': './src/index.tsx'
+		},
+		output: {
+			filename: '[name].main.js'
+		},
+		devtool: 'source-map',
+		resolve: {
+			extensions: [ '.ts', '.tsx', '.js', '.jsx' ]
+		},
+		module: {
+			rules: [
+				{
+					test: /\.tsx?$/,
+					loader: "ts-loader",
+				}
+			]
+		},
+		plugins: [
+			new HtmlWebpackPlugin({
+				template: 'src/index.html',
+				templateParameters: {
+					title: 'Crypto Bot'
+				},
+				hash: true,
+			}),
+		]
+	}
+}
